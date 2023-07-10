cdcb08266d8beba69bd8600f27c5ef2ca9fbdcb5
kevbesset <kevbesset@gmail.com>
2023-07-10 11:28:49 +0200

    update libs

--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+  import { useI18n } from 'vue-i18n'
+
+  const { t } = useI18n()
+</script>
+
 <template>
-  <div class="error">404 not found</div>
+  <div class="error">{{ t('title') }}</div>
 </template>
+
+<i18n lang="json">
+{
+  "fr": {
+    "title": "404 page introuvable"
+  },
+  "en": {
+    "title": "404 page not found"
+  }
+}
+</i18n>
