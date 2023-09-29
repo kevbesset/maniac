43c1282b8a3a92b726a59bf2ceebef7156b86453
Kevin <kevin.besset@jolimoi.com>
2023-09-29 12:59:43 +0200

    appear directive with possible options

--- src/directives/appear.ts
+++ src/directives/appear.ts
@@ -1,8 +1,26 @@
 import { DirectiveBinding } from 'vue'
 
+interface AppearOptions {
+  animationName?: string
+  duration?: 'slow' | 'slower' | 'fast' | 'faster'
+}
+
 export const appear = (el: HTMLElement, bindings: DirectiveBinding) => {
-  const animation = bindings.value || 'fadeInUp'
-  el.classList.add(`animate__${animation}`)
-  el.classList.add('animate__slow')
+  let animationName
+  let options: AppearOptions = {}
+
+  if (typeof bindings.value === 'undefined') {
+    animationName = 'fadeInUp'
+  } else if (typeof bindings.value === 'string') {
+    animationName = bindings.value
+  } else {
+    animationName = bindings.value.animation
+    options = bindings.value
+  }
+
+  el.classList.add(`animate__${animationName}`)
+  if (options?.duration) {
+    el.classList.add(`animate__${options.duration}`)
+  }
   el.classList.add('animate__animated')
 }
