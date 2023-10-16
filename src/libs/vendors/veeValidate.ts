ac15c7ea4c12834ed848804321d3111841152ea2
Kevin <kevin.besset@jolimoi.com>
2023-10-16 17:43:33 +0200

    fix maniac

--- src/libs/vendors/veeValidate.ts
+++ src/libs/vendors/veeValidate.ts
@@ -1,5 +1,5 @@
 import { configure, defineRule } from 'vee-validate'
-import AllRules from '@vee-validate/rules'
+import * as AllRules from '@vee-validate/rules'
 import { localize } from '@vee-validate/i18n'
 import en from '@vee-validate/i18n/dist/locale/en.json'
 import fr from '@vee-validate/i18n/dist/locale/fr.json'
